e54d7e29f1d556351c860931fafd9112872e37bf
bigmurtaza <muratcansarkalkan@gmail.com>
2022-02-25 23:58:45 +0300

    Updates 25.2.2022

--- main.py
+++ main.py
@@ -2,26 +2,33 @@
 # Main code.
 import py7zr
 import os
+import pandas as pd
+
+# Conversion of team database to a dictionary. Not yet implemented
+# def listofteams(list):
+#     df = pd.read_excel(list)
+#     df = df.iloc[ : , 1:]
+#     d = (df.set_index('Team').T.to_dict('records')[0])
 
 # Used for retrieving the available stadiums.
-def retrievelist(path):
+def retrievelist(pathnew):
     # We used scandir instead of listdir. os.listdir(path) gives str. os.scandir(path) gave list instead
     # If scanned paths are a directory then the name will be given.
-    list = ([f.name for f in os.scandir(path+"\data\stadium\FIFA") if f.is_dir()])
+    list = ([f.name for f in os.scandir(pathnew) if f.is_dir()])
     return list
 
 # Fine, you'll specify only main path.
 
 # Used for packing a folder with a parent folder. mode r is for read. w is for write
-def pack(list):
+def pack(list,pathnew):
     for ID in list:
-        # Example; if ID starts with 0001, then only they will be packed. Still WIP
-        if ID.startswith("0001") == True:
+        # Now, if the complete.txt is included in the folder, then it will be packed.
+        if os.path.isfile(f'{pathnew}/{ID}/complete.txt') == True:
             with py7zr.SevenZipFile(f'{ID}.7z', mode = "w") as archive:
-                archive.writeall(f"{ID}", f"data\stadium\FIFA\{ID}")
+                archive.writeall(f"{pathnew}/{ID}", f"data\stadium\FIFA\{ID}")
 
 # Progress 25.2.2022: Now we are able to pack what we want and have what we want.
-# Next: I will pack 7z's that has the empty file "complete.txt"
+# Progress 25.2.2022: I can pack 7z's that has the empty file "complete.txt"
 
 # # Used for extracting a folder.
 # def unpack(ID):
@@ -30,6 +37,7 @@ def pack(list):
 
 # Main function.
 if __name__ == "__main__":
-    path=input("Specify the folder that you installed the game")
-    list = retrievelist(path)
-    export = pack(list)
+    pathbase=input("Specify the folder that you installed the game")
+    pathnew=pathbase+"\data\stadium\FIFA"
+    list = retrievelist(pathnew)
+    export = pack(list,pathnew)
